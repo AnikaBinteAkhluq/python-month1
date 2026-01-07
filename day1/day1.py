3bd408541c70eff2ef51165bf4e098defb8c988a
AnikaBinteAkhluq <anikabinteakhluq011@gmail.com>
2026-01-07 20:09:23 +0600

    Day 1: extra practice added

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,40 @@
+print("Hello World!")
+print(6)
 print("My first python program")
 print("Day 1 completed")
+print(2*3)
+
+#variable=like container hold data
+a1=1
+print(a1)
+print("a1")
+
+#datatype=specify type of value a variable holds
+#numeric=int,float,complex
+#text data= str
+#boolean=True,False
+
+a=1
+b=True
+c="Anika"
+d=None
+e=1.1
+f=complex(2,3)
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
+print("The type of e is",type(e))
+print("The type of f is",type(f))
+
+#sequenced data=list(collection of different data element),tuple(as like list but immutable means not changeble)
+list1 = [1, 1.2, [-3,4], ["Anika","Akhluq"]]
+print(list1)
+
+tuple1 = (("lion","tiger"),("anik","anika"))
+print(tuple1)
+
+
+#Mapped data = dict
+dict1 = {"name":"anika", "age":22, "canVote":True}
+print(dict1)
