47e09881a1c41c5086d9aace315bd87cba85fef0
AnikaBinteAkhluq <anikabinteakhluq011@gmail.com>
2026-01-08 13:01:04 +0600

    Day 2: comments,take inputs method completed

---
+++ day2/day2.py
@@ -0,0 +1,33 @@
+#single line comment
+'''
+hello world       multiple line comment  
+'''     
+print("i am a girl\nand i read in class 6")  #newline  (escape sequence character)     
+"""
+hello world
+"""
+print("I am a \"good boy\"\nand i read in class 5 ") #"good boy"(escape sequence character)
+
+print("hello",5,6,sep="~",end="009\n") #more print statement
+print("Anika")
+
+
+a=input("Enter your name: ")  #take input
+print(a)
+
+X=input("Enter your first number: ")
+Y=input("Enter your second number: ")
+
+print(X+Y) #concatenation
+print( int(X) + int(Y) ) #arithmetic operation
+
+#practice
+#print(X*Y) #string*string invalid
+print( int(X) * int(Y) )
+
+#print(X-Y) invalid
+print( int(X) - int(Y) )
+
+#print(X/Y) invalid
+print( int(X) / int(Y) )
+
