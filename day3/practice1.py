971d47c33e2b30cee05560856268fe6da760f3ff
AnikaBinteAkhluq <anikabinteakhluq011@gmail.com>
2026-01-10 18:52:56 +0600

    day 3: all practices are added

---
+++ day3/practice1.py
@@ -0,0 +1,51 @@
+# Day 1 -intro,setup,Hello world! related problem solve
+
+print("Hello,World!")
+print("Name:Anika","age:23\n")
+print("i am learning python\n","Day 1 completed")
+print("*")
+print("**")
+print("***")
+
+# Day 2- variables & Data types practice problem
+name="anika"
+print(name)
+age=21
+print(age)
+
+a=2
+b=3
+print(a+b)
+print(a*b)
+
+price=1000
+quantity=500
+total=(price*quantity)
+print("total cost: ",total)
+
+d=True
+print(d)
+
+e=5.4
+print(e)
+
+f=12
+print(f)
+f=20
+print(f)
+
+h=1
+i=2
+k=3
+total=(h+i+k)
+average=(total/3)
+print(average)
+
+l=1
+m=2.3
+n="python"
+print(type(l))
+print(type(m))
+print(type(n))
+
+
